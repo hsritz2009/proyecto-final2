37c5c2e08ecc094e82ca414e1e749e614c848d5b
hsritz2009 <98743112+hsritz2009@users.noreply.github.com>
2022-06-01 09:27:33 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.magneticForce(Dimension.Strength))
+})
+basic.forever(function () {
+    if (input.magneticForce(Dimension.Strength) < 200) {
+        basic.showIcon(IconNames.StickFigure)
+    }
+})
